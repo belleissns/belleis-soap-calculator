4f888ec27b64cf9ee3a7e4f3b1d31954ceb45999
belleissns <belleis.sns@gmail.com>
2025-09-09 00:25:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/belleis-soap-calculator/',   // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
 })
